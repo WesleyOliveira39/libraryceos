d5dd6b1760efc17b05e31746abc59aee15b6d74e
Wesley Henrique <wesley.oliveira39@fatec.sp.gov.br>
2022-11-26 09:05:49 -0300

    staticfiles in

--- LibraryCeos/settings.py
+++ LibraryCeos/settings.py
@@ -25,13 +25,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
     'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django.contrib.humanize',
     
     'allauth',
