22e4d7ff958f6c0f4d93c91156de1d3915f9dc7b
Wesley Henrique <wesley.oliveira39@fatec.sp.gov.br>
2022-11-26 08:55:43 -0300

    finally?

--- LibraryCeos/settings.py
+++ LibraryCeos/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 
 #Arquivos > Uploads
