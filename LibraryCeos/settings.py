5293f3d71526be3977cfd8d490bce7ab47315a53
Wesley Henrique <wesley.oliveira39@fatec.sp.gov.br>
2022-11-28 03:04:02 -0300

    redirect https

--- LibraryCeos/settings.py
+++ LibraryCeos/settings.py
@@ -67,6 +67,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
+
 ROOT_URLCONF = 'LibraryCeos.urls'
 
 TEMPLATES = [
