0ecb28776bdbb5a548e77acc057beec4b45c09a2
Wesley Henrique <wesley.oliveira39@fatec.sp.gov.br>
2022-11-28 05:06:06 -0300

    no allowed

--- LibraryCeos/settings.py
+++ LibraryCeos/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'dn4!q!tvvyh7qy$wf**l7j#i^th-nd#%nul=3$kp+!59c%&=@!'
 DEBUG = config('DEBUG', cast=bool, default=False)
 
 
-ALLOWED_HOSTS = [".herokuapp.com"]  # ALTERAR EM CASO DE NOVA HOSPEDAGEM
+ALLOWED_HOSTS = []  # ALTERAR EM CASO DE NOVA HOSPEDAGEM
 
 #RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 #if RENDER_EXTERNAL_HOSTNAME:    
