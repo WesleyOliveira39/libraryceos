5e54ecd4d33b202a948f8edf2b2da9ab5a94d90e
Wesley Henrique <wesley.oliveira39@fatec.sp.gov.br>
2022-12-07 05:56:18 -0300

    asgi

--- LibraryCeos/asgi.py
+++ LibraryCeos/asgi.py
@@ -7,6 +7,8 @@ from django.core.asgi import get_asgi_application
 import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django_asgi_app = get_asgi_application()
+
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
